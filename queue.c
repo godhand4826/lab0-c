60653ab3e329906bfd25db0581bb54073d525495
huangyaozheng <godhand4826@gmail.com>
2024-02-29 00:23:15 +0800

    Implement bacic queue operations

--- queue.c
+++ queue.c
@@ -1,8 +1,13 @@
+#include "queue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "list.h"
 
-#include "queue.h"
+typedef struct {
+    struct list_head list;
+    int size;
+} queue_t;
 
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
  * but some of them cannot occur. You can suppress them by adding the
@@ -10,44 +15,161 @@
  *   cppcheck-suppress nullPointer
  */
 
-
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    queue_t *q = malloc(sizeof(queue_t));
+    if (q == NULL) {
+        return NULL;
+    }
+
+    INIT_LIST_HEAD(&q->list);
+    q->size = 0;
+
+    return &q->list;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (head == NULL) {
+        return;
+    }
+
+    queue_t *q = list_entry(head, queue_t, list);
+
+    element_t *node, *safe;
+    list_for_each_entry_safe (node, safe, head, list) {
+        list_del(&(node->list));
+        q_release_element(node);
+    }
+
+    free(q);
+}
 
 /* Insert an element at head of queue */
+// cppcheck-suppress constParameterPointer
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (head == NULL) {
+        return false;
+    }
+
+    size_t len = strlen(s);
+    char *value = malloc((len + 1) * sizeof(char));
+    if (value == NULL) {
+        return NULL;
+    }
+    strncpy(value, s, len);
+    value[len] = '\0';
+
+    element_t *node = malloc(sizeof(element_t));
+    if (node == NULL) {
+        free(value);
+        return NULL;
+    }
+    INIT_LIST_HEAD(&node->list);
+    node->value = value;
+
+    queue_t *q = list_entry(head, queue_t, list);
+    list_add(&(node->list), &(q->list));
+    q->size += 1;
+
+    // cppcheck-suppress memleak
     return true;
 }
 
 /* Insert an element at tail of queue */
+// cppcheck-suppress constParameterPointer
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (head == NULL) {
+        return false;
+    }
+
+    size_t len = strlen(s);
+    char *value = malloc((len + 1) * sizeof(char));
+    if (value == NULL) {
+        return NULL;
+    }
+    strncpy(value, s, len);
+    value[len] = '\0';
+
+    element_t *node = malloc(sizeof(element_t));
+    if (node == NULL) {
+        free(value);
+        return NULL;
+    }
+    INIT_LIST_HEAD(&node->list);
+    node->value = value;
+
+    queue_t *q = list_entry(head, queue_t, list);
+    list_add_tail(&(node->list), &(q->list));
+    q->size += 1;
+
+    // cppcheck-suppress memleak
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL) {
+        return NULL;
+    }
+
+    queue_t *q = list_entry(head, queue_t, list);
+
+    if (list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *node = list_first_entry(head, element_t, list);
+    list_del_init(&(node->list));
+    q->size -= 1;
+
+    if (sp != NULL) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL) {
+        return NULL;
+    }
+    queue_t *q = list_entry(head, queue_t, list);
+
+    if (q->size == 0) {
+        return NULL;
+    }
+
+    element_t *node = list_last_entry(head, element_t, list);
+    list_del_init(&(node->list));
+    q->size -= 1;
+
+    if (sp != NULL) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return node;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (head == NULL) {
+        return 0;
+    }
+
+    queue_t const *q = list_entry(head, queue_t, list);
+
+    return q->size;
 }
 
 /* Delete the middle node in queue */
