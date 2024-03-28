0cddb8f1af16c92d3a6007f79ebd0dd6dc09131e
huangyaozheng <godhand4826@gmail.com>
2024-03-29 00:21:16 +0800

    Add pre-process measurements prior to statistical processing

--- dudect/fixture.c
+++ dudect/fixture.c
@@ -42,6 +42,7 @@
 
 #define ENOUGH_MEASURE 10000
 #define TEST_TRIES 10
+#define DUDECT_NUMBER_PERCENTILES (100)
 
 static t_context_t *t;
 
@@ -64,7 +65,35 @@ static void differentiate(int64_t *exec_times,
         exec_times[i] = after_ticks[i] - before_ticks[i];
 }
 
-static void update_statistics(const int64_t *exec_times, uint8_t *classes)
+static int cmp(const int64_t *a, const int64_t *b)
+{
+    if (*a == *b)
+        return 0;
+    return (*a > *b) ? 1 : -1;
+}
+
+/* set different thresholds for cropping measurements.
+ * the exponential tendency is meant to approximately match
+ * the measurements distribution, but there's not more science
+ * than that.
+ */
+static void prepare_percentiles(int64_t *exec_times, int64_t *percentiles)
+{
+    qsort(exec_times, N_MEASURES, sizeof(int64_t),
+          (int (*)(const void *, const void *)) cmp);
+
+    for (size_t i = 0; i < DUDECT_NUMBER_PERCENTILES; i++) {
+        double which =
+            (1 - (pow(0.5, 10 * (double) (i + 1) / DUDECT_NUMBER_PERCENTILES)));
+        size_t index = (size_t) ((double) N_MEASURES * (double) which);
+        assert(index < N_MEASURES);
+        percentiles[i] = exec_times[index];
+    }
+}
+
+static void update_statistics(const int64_t *exec_times,
+                              const int64_t *percentiles,
+                              uint8_t *classes)
 {
     for (size_t i = 0; i < N_MEASURES; i++) {
         int64_t difference = exec_times[i];
@@ -74,6 +103,21 @@ static void update_statistics(const int64_t *exec_times, uint8_t *classes)
 
         /* do a t-test on the execution time */
         t_push(t, difference, classes[i]);
+
+        // t-test on cropped execution times, for several cropping thresholds.
+        for (size_t crop_index = 0; crop_index < DUDECT_NUMBER_PERCENTILES;
+             crop_index++) {
+            if (difference < percentiles[crop_index]) {
+                t_push(t, difference, classes[i]);
+            }
+        }
+
+        // second-order test (only if we have more than 10000 measurements).
+        // Centered product pre-processing.
+        if (t->n[0] > 10000) {
+            double centered = (double) difference - t->mean[classes[i]];
+            t_push(t, centered * centered, classes[i]);
+        }
     }
 }
 
@@ -121,11 +165,12 @@ static bool doit(int mode)
     int64_t *before_ticks = calloc(N_MEASURES + 1, sizeof(int64_t));
     int64_t *after_ticks = calloc(N_MEASURES + 1, sizeof(int64_t));
     int64_t *exec_times = calloc(N_MEASURES, sizeof(int64_t));
+    int64_t *percentiles = calloc(N_MEASURES + 1, sizeof(int64_t));
     uint8_t *classes = calloc(N_MEASURES, sizeof(uint8_t));
     uint8_t *input_data = calloc(N_MEASURES * CHUNK_SIZE, sizeof(uint8_t));
 
-    if (!before_ticks || !after_ticks || !exec_times || !classes ||
-        !input_data) {
+    if (!before_ticks || !after_ticks || !exec_times || !percentiles ||
+        !classes || !input_data) {
         die();
     }
 
@@ -133,12 +178,14 @@ static bool doit(int mode)
 
     bool ret = measure(before_ticks, after_ticks, input_data, mode);
     differentiate(exec_times, before_ticks, after_ticks);
-    update_statistics(exec_times, classes);
+    prepare_percentiles(exec_times, percentiles);
+    update_statistics(exec_times, percentiles, classes);
     ret &= report();
 
     free(before_ticks);
     free(after_ticks);
     free(exec_times);
+    free(percentiles);
     free(classes);
     free(input_data);
 
@@ -170,8 +217,11 @@ static bool test_const(char *text, int mode)
     return result;
 }
 
-#define DUT_FUNC_IMPL(op) \
-    bool is_##op##_const(void) { return test_const(#op, DUT(op)); }
+#define DUT_FUNC_IMPL(op)                \
+    bool is_##op##_const(void)           \
+    {                                    \
+        return test_const(#op, DUT(op)); \
+    }
 
 #define _(x) DUT_FUNC_IMPL(x)
 DUT_FUNCS
